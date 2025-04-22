d1b16416bc136c2e793dc52a762d171c64f92d31
aemioo2500 <nipon.aemioo@gmail.com>
2025-04-22 08:18:03 +0700

    Delete api/utils directory

--- api/utils/serial_utils.py
+++
@@ -1,7 +0,0 @@
-import serial
-
-def send_command(command: str) -> str:
-    with serial.Serial('COM4', baudrate=9600, timeout=1) as ser:
-        ser.write(bytes.fromhex(command))
-        response = ser.read(64)
-        return response.hex().upper()
